8c49b919f1add27675485992beda3ed14addc694
sandypockets <zac@znacreative.com>
2021-04-11 01:19:36 -0400

    Adjust routes to use custom CSS

--- express_server.js
+++ express_server.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public/javascript')));
+app.use(express.static(path.join(__dirname, 'public/styles')));
 app.use(morgan('short'));
 app.use(cookieSession({
   name: 'session',
